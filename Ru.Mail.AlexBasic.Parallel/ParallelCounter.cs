4eccc8c1eed42f0c483c9e2a947198606e7d563f
Aleksandr Vladimirovich <alex_basic@mail.ru>
2021-11-02 19:25:53 +0500

    Move type to file

---
+++ Ru.Mail.AlexBasic.Parallel/ParallelCounter.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+
+namespace Ru.Mail.AlexBasic.Parallel
+{
+    public class ParallelCounter
+    {
+        private readonly object _syncObject = new object();
+
+        private int _counter;
+
+        public int Increment()
+        {
+            var taskOne = Task.Run(Inc);
+            var taskTwo = Task.Run(Inc);
+
+            taskOne.Wait();
+            taskTwo.Wait();
+
+            return _counter;
+        }
+
+        public async Task<int> IncrementAsync()
+        {
+            var taskOne = Task.Run(Inc);
+            var taskTwo = Task.Run(Inc);
+
+            await taskOne;
+            await taskTwo;
+
+            return _counter;
+        }
+
+        private void Inc()
+        {
+            for (var i = 0; i < 1000000; i++)
+            {
+                lock (_syncObject)
+                {
+                    _counter++;
+                }
+            }
+        }
+    }
+}
--- Ru.Mail.AlexBasic.Parallel/Program.cs
+++ Ru.Mail.AlexBasic.Parallel/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 
 namespace Ru.Mail.AlexBasic.Parallel
 {
@@ -12,46 +11,6 @@ namespace Ru.Mail.AlexBasic.Parallel
 
             Console.WriteLine($"Result: {resultOne}");
             Console.WriteLine($"Result: {resultTwo}");
-        }
-    }
-
-    public class ParallelCounter
-    {
-        private readonly object _syncObject = new object();
-
-        private int _counter;
-
-        public int Increment()
-        {
-            var taskOne = Task.Run(Inc);
-            var taskTwo = Task.Run(Inc);
-
-            taskOne.Wait();
-            taskTwo.Wait();
-
-            return _counter;
-        }
-
-        public async Task<int> IncrementAsync()
-        {
-            var taskOne = Task.Run(Inc);
-            var taskTwo = Task.Run(Inc);
-
-            await taskOne;
-            await taskTwo;
-
-            return _counter;
-        }
-
-        private void Inc()
-        {
-            for (var i = 0; i < 1000000; i++)
-            {
-                lock (_syncObject)
-                {
-                    _counter++;
-                }
-            }
         }
     }
 }
